92b659463e416ef0924d9dfe3d262f3d45821718
lican <lican.m.olea@gmail.com>
2022-05-02 12:13:24 -0400

    apolleta funcionando

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hola inmundo")
+var ampolleta = document.getElementById("ampolleta")
+var botonOff = document.getElementById("apagar")
+var botonOn = document.getElementById("encender")
+
+botonOn.addEventListener("click",function(){
+    console.log("encender")
+    ampolleta.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+})
+
+botonOff.addEventListener("click",function(){
+    console.log("apagar")
+    ampolleta.src = "https://www.w3schools.com/js/pic_bulboff.gif"
+})
